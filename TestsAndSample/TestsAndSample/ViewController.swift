be4c12c949c09bcccc542caee356105873f9e071
Seivan Heidari <seivan.heidari@icloud.com>
2014-07-22 04:12:17 +0200

    Add toch event.

--- TestsAndSample/TestsAndSample/ViewController.swift
+++ TestsAndSample/TestsAndSample/ViewController.swift
@@ -22,10 +22,6 @@ class __AnimeraInternalWrapper : NSObject {
   }
   
   func update(displayLink:CADisplayLink) {
-//    println(CACurrentMediaTime())
-//    println(displayLink.timestamp)
-//    println("\n")
-//    println("\n")
     println("\n")
     let currentDuration = displayLink.timestamp - self.startTime
     println(currentDuration)
@@ -54,7 +50,6 @@ struct Animera {
     __AnimeraInternalManager.map[displayLink] = internal
     displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
     
-    
   }
   
   
@@ -62,8 +57,8 @@ struct Animera {
 
 class ViewController: UIViewController {
   @IBOutlet var box:UIView
-  var previousCenter:CGPoint?
   var newCenter:CGPoint?
+  var previousCenter:CGPoint?
   var duration = 1.5
   var previousCornerRadius:Double?
   var newCornerRadius:Double?
@@ -75,13 +70,14 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
     
-    self.previousCenter = self.box.center
+
     self.previousCornerRadius = self.box.layer.cornerRadius
     self.previousSize = self.box.frame.size
     let random = Double.sh_random(min: 25, max: 100)
     self.newSize = CGSize(width: random, height: random)
     self.newCenter = CGPoint(x: 320.sh_randomFromZero, y: 480.sh_randomFromZero)
     self.newCornerRadius = 30.sh_randomFromZero
+    self.previousCenter = self.box.center
     
     
     
@@ -93,7 +89,8 @@ class ViewController: UIViewController {
       self.update()
       
     }
-    
+    let tap = UITapGestureRecognizer(target: self, action: "didTap:")
+    self.view.addGestureRecognizer(tap)
     
   }
   
@@ -103,31 +100,37 @@ class ViewController: UIViewController {
   }
   
   
-  
+  func didTap(tap:UITapGestureRecognizer) {
+    if(tap.state == .Ended) {
+      self.newCenter = tap.locationInView(self.view)
+      self.update()
+    }
+  }
   func update() {
     
     
     Animera.runWithDuration(self.duration) { progress, reversedProgress, isFinished in
-
       let timedPercentage = self.timingFunctionHandler(progress)
-      self.box.center.x = (self.newCenter!.x * timedPercentage) + (self.previousCenter!.x * (1.0-timedPercentage))
-      self.box.center.y =  (self.newCenter!.y * timedPercentage) + (self.previousCenter!.y * (1.0-timedPercentage))
-      
-      self.box.bounds.size.width = (self.newSize!.width * timedPercentage) + (self.previousSize!.width * (1.0-timedPercentage))
-      self.box.bounds.size.height =  (self.newSize!.height * timedPercentage) + (self.previousSize!.height * (1.0-timedPercentage))
+//      self.box.center.x = (self.newCenter!.x * timedPercentage) + (self.box.center.x * (1.0-timedPercentage))
+//      self.box.center.y =  (self.newCenter!.y * timedPercentage) + (self.box.center.y * (1.0-timedPercentage))
+
+      self.box.center.x = (self.newCenter!.x * timedPercentage) + (self.box.center.x * (1.0-timedPercentage))
+      self.box.center.y =  (self.newCenter!.y * timedPercentage) + (self.box.center.y * (1.0-timedPercentage))
+
+//      self.box.bounds.size.width = (self.newSize!.width * timedPercentage) + (self.previousSize!.width * (1.0-timedPercentage))
+//      self.box.bounds.size.height =  (self.newSize!.height * timedPercentage) + (self.previousSize!.height * (1.0-timedPercentage))
       
       self.box.layer.cornerRadius = (self.newCornerRadius! * timedPercentage) + (self.previousCornerRadius! * (1.0-timedPercentage))
       if(isFinished == true) {
         self.timingFunctionHandler = TimingFunctions.randomTimingFunction
-        self.previousCenter = self.box.center
         self.previousCornerRadius = self.newCornerRadius
         self.previousSize = self.newSize
         self.newCornerRadius = 20.sh_randomFromZero
-        self.newCenter = CGPoint(x: 320.sh_randomFromZero, y: 480.sh_randomFromZero)
-        
+//        self.newCenter = CGPoint(x: 320.sh_randomFromZero, y: 480.sh_randomFromZero)
+        self.previousCenter = self.box.center
         let random = Double.sh_random(min: 25, max: 100)
         self.newSize = CGSize(width: random, height: random)
-        self.update()
+//        self.update()
         
       }
       
